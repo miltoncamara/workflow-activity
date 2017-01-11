e7a63451fb4c61c13e9ca6ad6107a2dcbeac3458
macOS <macos@Mac-mini-de-macOS.local>
2017-01-10 22:57:58 -0200

    Excluindo o plugin CSS MIn uma vez que o GULP SASS já dispoe da funcionalidade.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,25 @@
 var gulp = require("gulp");
 var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
 var htmlmin = require('gulp-htmlmin');
-var watch = require('gulp-watch');
 var del = require('del');
 
-gulp.task("clean", function() {
+gulp.task("clean", function () {
     del('./dist')
 });
 
-gulp.task("sass", ["clean"], function() {
+gulp.task("sass", ["clean"], function () {
     return gulp.src("./source/scss/**/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest("./dist/css/"));
 });
 
-gulp.task("html", ["clean"], function() {
+gulp.task("html", ["clean"], function () {
     return gulp.src("./source/**/*.html")
-        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task("background", function() {
+gulp.task("background", function () {
     gulp.watch("./source/scss/*.scss", ["sass"]);
     gulp.watch("./source/**/*.html", ["html"]);
 });
